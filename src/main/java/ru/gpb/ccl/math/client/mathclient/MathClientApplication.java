cf9cbbd859130284ed58a571227717c02aedfa89
Oleg.Chumin <oleg.tschumin@gmail.com>
2024-11-29 15:43:21 +0300

    Повышена версия библиотеки metrucs-common до 1.0.0.75

--- src/main/java/ru/gpb/ccl/math/client/mathclient/MathClientApplication.java
+++ src/main/java/ru/gpb/ccl/math/client/mathclient/MathClientApplication.java
@@ -4,7 +4,6 @@ package ru.gpb.ccl.math.client.mathclient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Slf4j
 @SpringBootApplication
